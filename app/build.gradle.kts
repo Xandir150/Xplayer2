326b5cd4693734e8354309dd002f3b0cd42189da
Alexander Shekhovtsov <aleksander.shekhovtsov@webpros.com>
2026-06-03 17:35:53 +0200

    chore(release): bump versionCode to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.teleteh.xplayer2"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
